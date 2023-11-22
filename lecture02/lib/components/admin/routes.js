a59bbdcb99db7b5488750d8dc82b5b2f3585ea84
Mottel <shachna@gmail.com>
2023-11-21 21:52:48 -0500

    Moved docs to yaml files and updated nodemon

--- lecture02/lib/components/admin/routes.js
+++ lecture02/lib/components/admin/routes.js
@@ -4,14 +4,7 @@ const db = require('../../core/database.js');
 
 /**
  * @swagger
- * /admin/db/init:
- *      get:
- *          description: Initializes the database
- *          responses:
- *              200:
- *                  description: Database initialized successfully
- *              500:
- *                  description: Error initializing database
+
  * 
  */
 router.get('/db/init', async (req, res) => {
